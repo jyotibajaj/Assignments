30ceaa9a5fe53a48fe604fa4092e98361c022a7c
Jyoti Bajaj <aashi.kwatra@gmail.com>
2014-06-10 00:06:58 -0700

    1. Indentation 2. Refactoring 3. Code cleaning

--- ProgrammingAssignment/src/practicecode/com/InputNumber.java
+++ ProgrammingAssignment/src/practicecode/com/InputNumber.java
@@ -4,27 +4,21 @@ import java.util.Scanner;
 
 public class InputNumber {
 	public int number;
-	
-	public int returnSum(int num){
-		int sum = 0; 
-		sum = num*(num +1)/2; // Sum numbers up to 10
-		System.out.println(sum); 
+
+	public int returnSum(int num) {
+		int sum = 0;
+		sum = num * (num + 1) / 2; // Sum numbers up to 10
 		return sum;
-//		int sum = num;
-//		for(int i = num; i>=1; i--){
-//			sum +=  i-1;
-//		}
-//		System.out.println(sum);
-//		return sum;
 	}
-	
+
 	public static void main(String[] args) {
-		InputNumber num1 = new InputNumber(); 
+		InputNumber num1 = new InputNumber();
 		Scanner s = new Scanner(System.in); // wait for the user input
 		System.out.println("Enter the number:");
-		 num1.number = s.nextInt();
-		 num1.returnSum(num1.number);
-		 
+		num1.number = s.nextInt();
+		int result = num1.returnSum(num1.number);
+		System.out.println(result);
+
 	}
 
 }
